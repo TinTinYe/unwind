83a503b567a5e622b3d21f660063969945853e4a
Yvonne <tintinye@gmail.com>
2019-12-17 10:40:04 +0800

    first

---
+++ unwind/Edit.swift
@@ -0,0 +1,14 @@
+//
+//  Edit.swift
+//  unwind
+//
+//  Created by Yvonne on 2019/12/17.
+//  Copyright © 2019 Yvonne. All rights reserved.
+//
+
+import Foundation
+struct AddIn {
+    var name:String
+    var beauty: Bool
+    var weight: Double
+}
